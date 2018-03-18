08957be6191b906e1bd73305d83280cd67aa88f4
Bogdans <b.ozerkins@gmail.com>
2018-03-18 15:32:52 +0200

    basic commands structure

---
+++ src/DataManagement/Command/TestCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DataManagement\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestCommand extends Command
+{
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ...
+    }
+}
